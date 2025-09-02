12672cffff1a6824dd63806b24e382bbd0e4d576
CVJourney <cvjourneycapeverde@gmail.com>
2025-09-02 19:39:34 -0100

    update all my file again to did one litle thing

---
+++ script_staff.js
@@ -0,0 +1,74 @@
+let db=null
+
+document.addEventListener("DOMContentLoaded",async function(){
+    await pegar()
+})
+
+async function pegar(){
+    let ler=await lerPosts()
+    let len=ler.length
+    let empresa=ler[len-1].empresa
+    let response=await fetch("https://cvpiramide.vercel.app/data_staff",{
+        method:"post",
+        headers:{
+            "content-type":"application/json"
+        },
+        body:JSON.stringify({empresa:empresa})
+    })
+    let res=await response.json()
+    console.log(res)
+    await trabalhar(res)
+}
+
+
+async function trabalhar(data){
+    data=data.reverse()
+    data.map((e)=>{
+        let html=`${e.tipo==true?'<h2>Aceito ✅</h2>':'<h2>Negado ❌⛔</h2>'}<p class="msg_n">Post: <strong>${e.nome}</strong> <br>Empresa: <strong>${e.empresa}</strong><br>Mensagem: <strong>${e.msg}</strong></p>`
+        let cria=document.createElement("div")
+        cria.classList.add("criado")
+        cria.innerHTML=html
+        document.body.appendChild(cria)
+    })
+}
+
+async function lerPosts() {
+  const db = await initDB();
+
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction(["post"], "readonly");
+    const store = transaction.objectStore("post");
+
+    const request = store.getAll();
+
+    request.onsuccess = function(event) {
+      resolve(event.target.result); // retorna todos os registros
+    };
+
+    request.onerror = function(event) {
+      reject(event.target.error);
+    };
+  });
+}
+
+async function initDB() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open("user_post", 1);
+
+    request.onupgradeneeded = function(event) {
+      const db = event.target.result;
+      if (!db.objectStoreNames.contains("post")) {
+        db.createObjectStore("post", { keyPath: "id", autoIncrement: true });
+      }
+    };
+
+    request.onsuccess = function(event) {
+      db = event.target.result;
+      resolve(db);
+    };
+
+    request.onerror = function(event) {
+      reject(event.target.error);
+    };
+  });
+}
